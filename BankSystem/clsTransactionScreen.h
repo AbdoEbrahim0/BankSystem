7c5f46bd3c80d8d06a082553fc231252700b7d13
AbdoEbrahim0 <abdelrahmanebrahim282@gmail.com>
2025-05-21 20:38:19 +0300

    adding header file Transacton screen

---
+++ BankSystem/clsTransactionScreen.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include "clsBankClientOnMyOwn.h"
+#include "clsUtil.h"
+#include "clsScreen.h"
+
+class clsTransactionScreen: protected clsScreen
+{
+private:
+
+public:
+	
+
+
+};
