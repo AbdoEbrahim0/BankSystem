f660f2a21c5cc9cf47c96eb662376f190d641235
AbdoEbrahim0 <abdelrahmanebrahim282@gmail.com>
2025-06-21 20:50:02 +0300

    [NaiveSol] for Register Logins in a Log File modify clsLoginScreen not clsUser we should pay attention to we care about data of user seperately then it must be in user class login screen must care only by login

--- BankSystem/clsDate.h
+++ BankSystem/clsDate.h
@@ -18,7 +18,7 @@ private:
 	short _Day = 1;
 	short _Month = 1;
 	short _Year = 1900;
-
+	short _Hour, _Minute, _Second;
 public:
 
 	clsDate()
@@ -28,6 +28,9 @@ public:
 		_Day = now->tm_mday;
 		_Month = now->tm_mon + 1;
 		_Year = now->tm_year + 1900;
+		_Hour = now->tm_hour;  // Current hour
+		_Minute = now->tm_min;   // Current minute
+		_Second = now->tm_sec;   // Current second
 	}
 
 	clsDate(string sDate)
@@ -150,10 +153,19 @@ public:
 	{
 		return  to_string(Date.Day) + "/" + to_string(Date.Month) + "/" + to_string(Date.Year);
 	}
-
+	
 	string DateToString()
 	{
 		return  DateToString(*this);
+	}
+
+	static string TimeToString(clsDate Date)
+	{
+		return  to_string(Date._Hour) + ":" + to_string(Date._Minute) + ":" + to_string(Date._Second);
+	}
+	string TimeToString()
+	{
+		return  TimeToString(*this);
 	}
 
 	static bool isLeapYear(short Year)
--- BankSystem/clsLoginScreen.h
+++ BankSystem/clsLoginScreen.h
@@ -8,12 +8,74 @@
 	  
 class clsLoginScreen: protected clsScreen
 {
-	 
-private:
 	
+private:
+	static string _LogDataToLine(string Usr, string pass,short perm,  string Seperator = "#//#")
+	{
+		string s;
+		clsDate currentDate;
+		s = s + clsDate::DateToString(currentDate) + " - ";
+		s = s + clsDate::TimeToString(currentDate) + Seperator;
+		s = s + Usr + Seperator;
+		s = s + pass + Seperator;
+		s = s +  to_string(perm);
+		return s;
+	}
+	static  vector <string> _LoadLogsDataFromFile()
+	{
+		vector <string> vLogs;
+		fstream MyFile;
+		MyFile.open("C:\\Users\\Mr_Abdo\\source\\repos\\BankSystem\\BankSystem\\LoginRegister.txt", ios::in);//read Mode
+		if (MyFile.is_open())
+		{
+			string Line;
+			while (getline(MyFile, Line))
+			{
+				vLogs.push_back(Line);
+			}
+			MyFile.close();
+		}
+		return vLogs;
+	}
+	static void _SaveLogsDataToFile(vector <string> vLogs)
+	{
+
+		fstream MyFile;
+		MyFile.open("C:\\Users\\Mr_Abdo\\source\\repos\\BankSystem\\BankSystem\\LoginRegister.txt", ios::out);//overwrite
+		string DataLine;
+		if (MyFile.is_open())
+		{
+			for (string log : vLogs)
+			{
+					//we only write records that are not marked for delete.  
+					DataLine = log;
+					MyFile << DataLine << endl;
+			}
+			MyFile.close();
+		}
+	}
+	static void _SaveLogDataToFile(string DataLineOfLoggedUser)
+	{
+
+		fstream MyFile;
+		MyFile.open("C:\\Users\\Mr_Abdo\\source\\repos\\BankSystem\\BankSystem\\LoginRegister.txt", ios::out);//overwrite
+
+		string DataLine;
+
+		if (MyFile.is_open())
+		{
+					//we only write records that are not marked for delete.  
+		DataLine = DataLineOfLoggedUser;
+		MyFile << DataLine << endl;
+		MyFile.close();
+
+		}
+
+	}
 	static bool _Login()
 	{
 		string UserName ,passWord ;
+		short PermissionsOfUser;
 		static short NumberOfFailedloggingIn=0;
 		bool LoginFailed = false;
 		do {	
@@ -34,10 +96,13 @@ private:
 			cout << "Enter password: \n";
 			cin >> passWord;
 			currentUser = clsUser::Find(UserName, passWord);
+			PermissionsOfUser = currentUser.Permissions;
 			LoginFailed = currentUser.IsEmpty();
 			
 		} while (LoginFailed );
-		
+		vector<string>myVLogs= _LoadLogsDataFromFile();
+		myVLogs.push_back(_LogDataToLine(UserName, passWord, PermissionsOfUser));
+		_SaveLogsDataToFile(myVLogs);
 		NumberOfFailedloggingIn = 0; // to reset number of failure when any user logged in successfully
 		clsMainScreen::ShowMainMenu();
 		return true;
